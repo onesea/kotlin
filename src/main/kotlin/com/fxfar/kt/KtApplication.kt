e4873cb5c6babaa0e2dbb482b1f0f5b229485c8e
ytl <tly@codein.tv>
2017-07-09 03:07:51 -0700

    first commit

---
+++ src/main/kotlin/com/fxfar/kt/KtApplication.kt
@@ -0,0 +1,11 @@
+package com.fxfar.Kt
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class KtApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(KtApplication::class.java, *args)
+}
